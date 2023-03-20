4451f40e12239ae6f1ea9d2a81e52112eb0cfa36
Tom Tuffin <ttuffin@redhat.com>
2023-03-20 21:10:32 +0100

    Switch to regular swaylock and remove bloat

--- bin/lock.sh
+++ bin/lock.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
-## Lockscreen script
-## Lockicon downloaded from https://creazilla.com/nodes/58783-locked-emoji-clipart
-## No modifications were made on the lockicon
-## Lockicon License: https://creativecommons.org/licenses/by/4.0/
+swaylock --daemonize \
+--indicator-radius 100 \
+--indicator-thickness 12 \
+--ring-color 2E3440 \
+--key-hl-color ECEFF4 \
+--line-color 88C0D0 \
+--inside-color ffffff00 \
+--separator-color 000000 \
+--inside-ver-color e7b41688 \
+--line-ver-color 88C0D0 \
+--ring-ver-color 2E3440 \
+--show-failed-attempts \
+-i ~/Pictures/Wallpaper/swaylock-2.jpg
 
-swaylock --daemonize --screenshots --clock --indicator \
-  --indicator-radius 100 \
-  --indicator-thickness 12 \
-  --effect-blur 7x5 \
-  --ring-color 2E3440 \
-  --key-hl-color ECEFF4 \
-  --line-color 88C0D0 \
-  --inside-color 00000088 \
-  --separator-color 00000000 \
-  --datestr %Y-%m-%d \
-  --text-color ECEFF4 \
-  --text-caps-lock-color ECEFF4 \
-  --show-failed-attempts \
-  --fade-in 0.1 \
-  --effect-scale 0.5 --effect-blur 8x3 --effect-scale 2 \
-  --effect-vignette 0.5:0.5 \
-  --effect-compose "1.5%,1.5%;-1x10%;$HOME/.config/sway/sway.d/lock.png"
