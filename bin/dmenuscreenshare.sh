454905007775c916b0740590099b765af5c1d5c9
Tom Tuffin <ttuffin@redhat.com>
2023-03-23 09:14:15 +0100

    Cleanup obsolete packages

--- bin/dmenuscreenshare.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-## Shows a dropdown menu to start, stop or view the status of screensharing
-
-case "$(echo -e " Start\n Stop\n Status" | bemenu-run.sh -l 4 -p "Screensharing:")" in
-" Start") screenshare.sh ;;
-" Stop") screenshare.sh stop ;;
-" Status") screenshare.sh is-recording ;;
-esac
